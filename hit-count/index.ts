07baa52da5c4c4a68635a20f6184d7661c879ed1
Yog <yog.panjarale@gmail.com>
2022-04-01 22:39:12 +0530

    hit counter working

--- hit-count/index.ts
+++ hit-count/index.ts
@@ -2,20 +2,26 @@ import { Application, Router } from "https://deno.land/x/oak@v10.5.1/mod.ts";
 import { oakCors } from "https://deno.land/x/cors@v1.2.2/mod.ts";
 
 
+const hits = {
+  times : 0,
+  lastTime : "",
+}
 const router = new Router();
 router
-  .get("/", (context) => {
-    context.response.body = "Hello World";
-  }).get("/time",(context)=>{
+  .get("/time",(context)=>{
     context.response.body=new Date().toLocaleTimeString("en-US",{timeZone:"Asia/Kolkata"});
   })
-  
-  
 
 const app = new Application();
 
 app.use(oakCors());
 app.use(router.routes());
 app.use(router.allowedMethods());
-
-app.listen();
+app.use((context) => { 
+  hits.times++;
+  hits.lastTime = new Date().toLocaleTimeString("en-US",{timeZone:"Asia/Kolkata"});
+  hits.times++;
+  hits.lastTime=new Date().toLocaleTimeString("en-US",{timeZone:"Asia/Kolkata"});
+  context.response.body=`${hits.times} times hit. Last hit at ${hits.lastTime}`;
+})
+app.listen({ port: 8000 });
